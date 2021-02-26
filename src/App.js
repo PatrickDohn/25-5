a886ffd9a62ded65d5eca977163643c76948f884
PatrickDohn <dohnpatrick1993@gmail.com>
2021-02-25 20:05:42 -0500

    finish reset button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import Timer from './components/Timer'
 import BreakTime from './components/BreakTime'
 import Countdown from './components/Countdown'
 
 function App() {
+  const audioElement = useRef(null)
   const [timer, setTimer] = useState(60 * 25)
   const [breakTimer, setbreakTimer] = useState(300)
   const [currentSessionType, setCurrentSessionType] = useState('Session')
@@ -31,7 +32,8 @@ const handleStartStop = () => {
                 if(currentSessionType === 'Session') {
                     setCurrentSessionType('Break')
                     setTimeLeft(breakTimer)
-                } else if(currentSessionType === 'Break') {
+                }
+                if(currentSessionType === 'Break') {
                     setCurrentSessionType('Session')
                     setTimeLeft(timer)
                 }
@@ -41,12 +43,27 @@ const handleStartStop = () => {
     }
 }
 
+const handleResetButtonClick = () => {
+
+    clearInterval(intervalId)
+
+    setIntervalId(null)
+
+    setCurrentSessionType('Session')
+
+    setTimer(60 * 25)
+
+    setbreakTimer(60 * 5)
+
+    setTimeLeft(60 * 25)
+}
+
 
   return (
     <div className="App">
       <Timer timer={timer} setTimer={setTimer} />
       <BreakTime breakTimer={breakTimer} setbreakTimer={setbreakTimer} />
-      <Countdown isStarted={isStarted} timeLeft={timeLeft} currentSessionType={currentSessionType} handleStartStop={handleStartStop} breakTimer={breakTimer} timer={timer} />
+      <Countdown isStarted={isStarted} timeLeft={timeLeft} currentSessionType={currentSessionType} handleStartStop={handleStartStop} breakTimer={breakTimer} timer={timer} handleResetButtonClick={handleResetButtonClick} audioElement={audioElement} />
     </div>
   );
 }
--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -5,7 +5,7 @@ import momentDurationFormatSetup from 'moment-duration-format'
 
 momentDurationFormatSetup(moment)
 
-function Countdown({ isStarted, timeLeft, currentSessionType, handleStartStop, handleReset }) {
+function Countdown({ isStarted, timeLeft, currentSessionType, handleStartStop, handleResetButtonClick, audioElement }) {
 
 
     const formatTimeLeft = moment.duration(timeLeft , 's').format('mm:ss', { trim: false})
@@ -17,7 +17,7 @@ function Countdown({ isStarted, timeLeft, currentSessionType, handleStartStop, h
             <div className="button-container">
                 <p>{currentSessionType}</p>
                 <button onClick={handleStartStop}>{isStarted ? 'Stop' : 'Start'}</button>
-                <button onClick={handleReset}>Reset</button>
+                <button onClick={handleResetButtonClick}>Reset</button>
             </div>
         </div>
     )
